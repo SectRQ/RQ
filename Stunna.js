2a3ebe32093098a907466fea14f44e096b6291b0
SectRQ <rachid2005286@gmail.com>
2022-01-06 11:02:38 +0100

    Update Stunna.js

--- Stunna.js
+++ Stunna.js
@@ -77,6 +77,8 @@ client.on("messageCreate", async (message) => {
     ///////// NO MUSIC COMMANDS ////////////////////
   let args = message.content.slice(config.prefix.length).trim().split(" ");
   let cmd = args.shift()?.toLowerCase();
+  let answers = [`${tMember.user} has been fucked by ${message.author.username}, but that nigga's pull out game STRONG.`, `${tMember.user} gave ${message.author.username} herpes.`, `${tMember.user} GAVE ${message.author} AIDS LMFAO`, `${tMember.user} has been impregnated by ${message.author.username} :pregnant_woman::skin-tone-5:`, `${tMember.user} got raped by this lame ahh nigga called ${message.author.username} :skull:`];
+  let randomA = answers[Math.floor(Math.random() * answers.length)];
   if (cmd === 'talk'){
     message.channel.send(`Yo ${message.author}, hoe gaat het bro?`);
   } else if (cmd === 'goed' | cmd === 'good'){
@@ -90,7 +92,7 @@ client.on("messageCreate", async (message) => {
         new Discord.MessageEmbed()
           .setColor('#C0A6A7')
           .setTitle('Fucked  :clown:')
-          .setDescription([`${tMember.user} has been fucked by ${message.author.username}, but that nigga's pull out game STRONG.`, `${tMember.user} gave ${message.author.username} herpes.`, `${tMember.user} GAVE ${message.author} AIDS LMFAO`, `${tMember.user} has been impregnated by ${message.author.username} :pregnant_woman::skin-tone-5:`, `${tMember.user} got raped by this lame ahh nigga called ${message.author.username} :skull:`])
+          .setDescription(randomA)
       ]
     })
   }
